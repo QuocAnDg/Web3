400bcee5b18dc86b6f3bfc7912851548394cbe0c
QuocAnDg <quocand40@gmail.com>
2023-12-25 17:00:22 +0100

    3.14

---
+++ exercices/module6/3.14/mongo.js
@@ -0,0 +1,35 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+const PhoneBook = require('./models/phoneBook')
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+app.get('/api/phoneBook', (request, response) => {
+    PhoneBook
+    .find({})
+    .then(phonesBook=> {
+        response.json(phonesBook)
+    })
+})
+
+app.post('/api/phoneBook', (request, response) => {
+    const body = request.body
+
+    if (body.phoneNumber === undefined || body.name === undefined) {
+        return response.status(400).json({ error: 'content missing' })
+    }
+    const phoneBook = new PhoneBook({
+        name: body.name,
+        phoneNumber: body.phoneNumber,
+    })
+    phoneBook.save().then(result => {
+        response.json(result)
+    })
+})
+
+
